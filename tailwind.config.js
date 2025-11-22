65a5b5c17823965f6fc4e77f47d4223735061f8d
Richard <mukechiwarichard@gmail.com>
2025-11-22 20:38:43 +0200

    "root commit message for site/index.html updates"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./site/**/*.{html,js}",      // All HTML and JS inside site/
-    "./site/**/*.html",           // In case nested HTML folders
-    "./site/styles/**/*.css"      // Include any other CSS you create
-  ],
+export default {
+  content: ["./site/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#0A1B2A",
-        accent: "#FF6A00"
-      },
-      fontFamily: {
-        inter: ["Inter", "sans-serif"]
-      }
-    }
-  },
-  plugins: [],
-};
+    extend: {}
+  }
+}
