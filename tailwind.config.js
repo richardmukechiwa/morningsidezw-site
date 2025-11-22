f1573a12f5a1f19697fea67cd117be26880230e5
Richard <mukechiwarichard@gmail.com>
2025-11-22 09:46:14 +0200

    "code update Update Tailwind CSS integration and configuration"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ["./site/**/*.{html,js}"],
   theme: {
     extend: {}
-  }
-}
+  },
+  plugins: [],
+};
